301aa3257516a51443f82b49f377669d191b8f5d
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-10-15 12:39:47 +0200

    Remove video from mobile screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,24 +48,26 @@ const Hero = () => {
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
 
-    const startValue = isMobile ? "top 50%" : "center 60%";
+    if (!isMobile) {
+      const startValue = isMobile ? "top 50%" : "center 60%";
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: "video",
-        start: startValue,
-        endTrigger: "#cocktails", // track the cocktails section's scroll position
-        end: "bottom bottom",
-        scrub: true,
-        pin: true,
-      },
-    });
-
-    videoRef.current.onloadedmetadata = () => {
-      tl.to(videoRef.current, {
-        currentTime: videoRef.current.duration,
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: "video",
+          start: startValue,
+          endTrigger: "#cocktails", // track the cocktails section's scroll position
+          end: "bottom bottom",
+          scrub: true,
+          pin: true,
+        },
       });
-    };
+
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current.duration,
+        });
+      };
+    }
   }, []);
 
   return (
@@ -106,15 +108,17 @@ const Hero = () => {
         </div>
       </section>
 
-      <div className="video absolute inset-0">
-        <video
-          ref={videoRef}
-          src="/videos/output.mp4"
-          muted
-          playsInline
-          preload="auto"
-        />
-      </div>
+      {!isMobile && (
+        <div className="video absolute inset-0">
+          <video
+            ref={videoRef}
+            src="/videos/output.mp4"
+            muted
+            playsInline
+            preload="auto"
+          />
+        </div>
+      )}
     </>
   );
 };
